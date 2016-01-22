00117ba00feb8a089da1e9a3da919f815575952c
Simon Gladman <sgladman@gmail.com>
2016-01-22 04:33:34 +0000

    Initial Commit

---
+++ CoreImageReductionFilterExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreImageReductionFilterExplorer
+//
+//  Created by Simon Gladman on 22/01/2016.
+//  Copyright © 2016 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
